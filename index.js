09595472eda3656117a5f20409d9269783d76a3d
Djordje Milojevic <georgemilojevic@gmail.com>
2019-01-27 16:00:41 +0100

    homework#1

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 var http = require('http');
+var url = require('url');
+var stringDecoder = require('string_decoder').StringDecoder;
 
-var httpServer = http.createServer(function(req, res){
-  runServer(req, res);
-  res.end("-> Hi! <-");
+var responseObject = {
+    "response": {
+        "success": false,
+        "code": 400,
+        "status": "Bad request",
+        "message": "Wrong HTTP method or wrong endpoint",
+    }
+}
 
+http.createServer(function(request, response){
+
+    var decoder = new stringDecoder('utf-8');
+    var payload = '';
+    var parsedUrl = url.parse(request.url, true);
+    var path = parsedUrl.pathname;
+    var responseMessage = {}
+    let body = [];
+
+    path == "/hello" && request.method.toLowerCase() === 'post' ?
+      responseObject = {"response": {"success": true, "code": 200,"status": "OK", "message": "Payload received"}}
+      :
+      responseObject
+
+    response.write(JSON.stringify(responseObject));
+    response.end();
+
+}).listen(8080, function() {
+      console.log('SERVER RUNNING')
 });
-
-httpServer.listen(3000, function(){
-  console.log('SERVER RUNNING')
-});
-
-
